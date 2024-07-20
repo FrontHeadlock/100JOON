596b4e3820c7a2250b8c7d8432c01eabb4dfe761
Dobby19 <150355097+FrontHeadlock@users.noreply.github.com>
2024-07-20 14:34:50 +0900

    Add files via upload

---
+++ 백준_1/JOON_10950.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open("input_1.txt","r")
+
+A = int(input())
+
+for i in range(0,A+1,1):
+    try:
+        X,Y=map(int,input().split())
+        print(f'{X+Y}')
+    except EOFError:
+        break;
